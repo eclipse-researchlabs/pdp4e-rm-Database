074d5180f248c2008c6cd580629994478bfe050f
Daniel Rudziński <danielrudzinski19@gmail.com>
2020-09-03 10:20:50 +0200

    Added VRT to relationships

--- QueryLanguages/RelationshipGraphQl.cs
+++ QueryLanguages/RelationshipGraphQl.cs
@@ -21,9 +21,49 @@ namespace Core.Database.QueryLanguages
             Field(x => x.Payload, true);
             //Field(x => x.RootId);
 
+            GetVulnerabilities();
+            GetRisks();
+            GetTreatments();
+
             Field(x => x.IsDeleted);
             Field(x => x.CreatedDateTime);
 
+        }
+
+        public FieldType GetRisks()
+        {
+            return Field<ListGraphType<RisksGraphQl>>(
+                name: "risks",
+                resolve: context =>
+                {
+                    var dbContext = (BeawreContext)context.UserContext;
+                    var relationships = dbContext.Relationship.Where(x => x.ToType == ObjectType.Risk && x.FromType == ObjectType.AssetEdge && x.FromId == context.Source.Id && !x.IsDeleted).Select(x => x.ToId).ToArray();
+                    return dbContext.Risk.Where(x => relationships.Contains(x.RootId) && !x.IsDeleted).ToList().GroupBy(x => x.RootId).Select(x => x.OrderByDescending(y => y.Version).FirstOrDefault());
+                });
+        }
+
+        public FieldType GetVulnerabilities()
+        {
+            return Field<ListGraphType<VulnerabilityGraphQl>>(
+                name: "vulnerabilities",
+                resolve: context =>
+                {
+                    var dbContext = (BeawreContext)context.UserContext;
+                    var relationships = dbContext.Relationship.Where(x => x.ToType == ObjectType.Vulnerabilitie && x.FromType == ObjectType.AssetEdge && x.FromId == context.Source.Id && !x.IsDeleted).Select(x => x.ToId).ToArray();
+                    return dbContext.Vulnerability.Where(x => relationships.Contains(x.RootId) && !x.IsDeleted).ToList().GroupBy(x => x.RootId).Select(x => x.OrderByDescending(y => y.Version).FirstOrDefault());
+                });
+        }
+
+        public FieldType GetTreatments()
+        {
+            return Field<ListGraphType<TreatmentsGraphQl>>(
+                name: "treatments",
+                resolve: context =>
+                {
+                    var dbContext = (BeawreContext)context.UserContext;
+                    var relationships = dbContext.Relationship.Where(x => x.ToType == ObjectType.Treatment && x.FromType == ObjectType.AssetEdge && x.FromId == context.Source.Id && !x.IsDeleted).Select(x => x.ToId).ToArray();
+                    return dbContext.Treatment.Where(x => relationships.Contains(x.RootId) && !x.IsDeleted).ToList().GroupBy(x => x.RootId).Select(x => x.OrderByDescending(y => y.Version).FirstOrDefault());
+                });
         }
     }
 }
