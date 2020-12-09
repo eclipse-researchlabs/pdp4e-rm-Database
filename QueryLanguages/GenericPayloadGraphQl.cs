8e41321375695ff193b2b19bc4275c2c5dab8473
Daniel Rudziński <danielrudzinski19@gmail.com>
2020-12-09 17:53:09 +0100

    Working on new documents list

---
+++ QueryLanguages/GenericPayloadGraphQl.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.Database.Payloads;
+using GraphQL.Types;
+
+namespace Core.Database.QueryLanguages
+{
+    public class GenericPayloadModel
+    {
+        public string Input { get; set; }
+        public string Output { get; set; }
+    }
+
+    public class GenericPayloadGraphQl : ObjectGraphType<GenericPayloadModel>
+    {
+        public GenericPayloadGraphQl()
+        {
+            Field<StringGraphType>("input", resolve: x => x.Source.Input);
+            Field<StringGraphType>("output", resolve: x => x.Source.Output);
+        }
+    }
+
+}
